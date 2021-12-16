13487847ff4f83c1defb995c07d74069c3d2faab
guilsa <guilsa001@gmail.com>
2021-12-15 23:02:30 -0800

    Add article groupings. WIP.

--- v1/frontend/src/App.js
+++ v1/frontend/src/App.js
@@ -1,21 +1,56 @@
 import React from 'react'
+import { useState } from 'react'
 import './App.css'
 
 import { useFetch } from './hooks/useFetch'
 
+function renderArticles(value, key, map) {
+  // console.log('value', value)
+  return value?.map((article) => {
+    console.log('article', article)
+    return (
+      <div key={article.id}>
+        <div className='box' style={{ textAlign: 'left', paddingBottom: 10 }}>
+          <h3>
+            <a href={article.url}>{article.title}</a>
+          </h3>
+          <p style={{ fontSize: '0.9em' }}>{article.description}</p>
+          <div style={{ fontSize: '1em', color: 'gray' }}>
+            <span>{article.source}</span>
+            <span style={{ textTransform: 'capitalize' }}>&nbsp;{renderBiasRating(article.bias_rating)}</span>
+          </div>
+        </div>
+      </div>
+    )
+  })
+}
+
 function App() {
   const { status, data, error } = useFetch('/articles')
+  const [articleGroup, setArticleGroup] = useState([])
 
-  const articles = data
+  React.useEffect(() => {
+    const groups = new Map()
+    data.forEach((article) => {
+      if (!groups.has(article.date)) {
+        groups.set(article.date, [article])
+      } else {
+        const stack = groups.get(article.date)
+        stack.push(article)
+      }
+      setArticleGroup(groups)
+    })
+  }, [data])
 
   return (
     <div>
-      <header style={{ marginLeft: 20 }}>{articles?.length} articles</header>
+      <header style={{ marginLeft: 20 }}>{data?.length} articles</header>
       <div className='App'>
         {status === 'error' && <div>{error}</div>}
         {status === 'fetching' && <span>Loading.....</span>}
-        {status === 'fetched' &&
-          articles?.map((article) => {
+        {status === 'fetched' && articleGroup.forEach(renderArticles)}
+        {/* {status === 'fetched' &&
+          articleGroups?.map((article) => {
             return (
               <div key={article.id}>
                 <div className='box' style={{ textAlign: 'left', paddingBottom: 10 }}>
@@ -32,7 +67,7 @@ function App() {
                 </div>
               </div>
             )
-          })}
+          })} */}
       </div>
     </div>
   )
