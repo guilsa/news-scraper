7fe845c20949ce2b618221bdc07620c56c0dc6e7
guilsa <guilsa001@gmail.com>
2021-12-20 13:06:26 -0800

    Update cron script

--- v1/cron.js
+++ v1/cron.js
@@ -2,16 +2,20 @@ const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 const cron = require('node-cron')
 
-cron.schedule('0 * * * *', function () {
-  console.log(`scrapping articles (${new Date().toLocaleString()})`)
+const everyHour = '0 * * * *'
+const everyFiveMins = '*/5 * * * *'
+
+cron.schedule(everyFiveMins, function () {
+  console.log(`scrapping articles (${new Date().toLocaleString()})...`)
   startArticleScraper()
 })
 
 async function startArticleScraper() {
   try {
     const { stdout, stderr } = await exec('node articlesScrapeSave.mjs')
-    console.log('stdout:', stdout)
-    console.log('stderr:', stderr)
+    console.table({ stdout, stderr })
+    //console.log('stdout:', stdout)
+    //console.log('stderr:', stderr)
   } catch (e) {
     console.error(e)
   }
