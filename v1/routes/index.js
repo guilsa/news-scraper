179596e54877df6a85e0e7ae4137b25c83d95833
guilsa <guilsa001@gmail.com>
2021-12-20 18:51:13 -0800

    Remove limit 50 from /articles for now

--- v1/routes/index.js
+++ v1/routes/index.js
@@ -10,7 +10,7 @@ router.get('/articles', function (req, res) {
     console.log('lastModified', lastModified)
     const articles = db
       .prepare(
-        'select articles.id, articles.title, articles.source, articles.description, articles.url, articles.date, articles.createdAt, lower(sources.bias_rating) as bias_rating from articles left join sources on articles."source" = sources."name" order by createdAt desc LIMIT 50'
+        'select articles.id, articles.title, articles.source, articles.description, articles.url, articles.date, articles.createdAt, lower(sources.bias_rating) as bias_rating from articles left join sources on articles."source" = sources."name" order by createdAt desc'
       )
       .all()
     res.send({
