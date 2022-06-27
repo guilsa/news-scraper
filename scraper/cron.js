71666d3bad5147abdb25bce15369a796c2f039ee
guilsa <guilsa001@gmail.com>
2022-06-27 10:12:21 -0700

    Fix cron script

--- scraper/cron.js
+++ scraper/cron.js
@@ -5,18 +5,20 @@ const cron = require('node-cron')
 const everyHour = '0 * * * *'
 const everyFiveMins = '*/5 * * * *'
 const everyThirtyMins = '*/30 * * * *'
+const everyMin = '* * * * *'
 
 cron.schedule(everyThirtyMins, function () {
-  console.log(`scraping articles (${new Date().toLocaleString()})...`)
+  console.log(`scraping...`)
   startArticleScraper()
 })
 
+const filepath = 'node ./src/services/articles.mjs'
+
 async function startArticleScraper() {
   try {
-    const { stdout, stderr } = await exec('node ./src/services/articles.mjs')
-    console.table({ stdout, stderr })
-    //console.log('stdout:', stdout)
-    //console.log('stderr:', stderr)
+    const { stdout, stderr } = await exec(filepath)
+    console.log(`${new Date().toISOString()}: ${stdout}`)
+    if (stderr) console.log('error:', stderr)
   } catch (e) {
     console.error(e)
   }
