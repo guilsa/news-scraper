73e1702c5e197bbd589ff909692c69f52a7ac5bb
guilsa <guilsa001@gmail.com>
2022-06-27 22:51:36 -0700

    Cheerio parse memeorandum

--- scraper/src/utils/parse/memeorandum.mjs
+++ scraper/src/utils/parse/memeorandum.mjs
@@ -1,19 +1,26 @@
 import cheerio from 'cheerio'
 
 function parseMemeorandum(data) {
+  const results = []
+
   let $ = cheerio.load(data)
   const item = $('.clus .item')
 
   for (let i = 0; i < item.length; i++) {
-    const title = $(item[i]).find('.ii a')
-    const citations = $(item[i]).find('.lnkr').text()
+    const title = $(item[i]).find('.ii a').text()
+    const citationElements = $(item[i]).find('.lnkr')
 
-    for (let j = 0; j < citations.length; j++) {
-      const source = $(citations[j]).find('cite a').text()
+    const citations = []
+
+    for (let j = 0; j < citationElements.length; j++) {
+      const name = $(citationElements[j]).find('cite a').text()
+      if (name.length > 0) citations.push(name)
     }
+
+    results.push({ title, citations })
   }
 
-  return { title, citations, source }
+  return results
 }
 
 export { parseMemeorandum }
